57b5def9c8b443b29939f36ea67e04c7b2aa228c
jasminakostadinovic <jasmina.kostadinovic26@gmail.com>
2020-08-03 19:45:14 +0200

    folder structure

---
+++ Company_app/ViewModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Company_app.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
